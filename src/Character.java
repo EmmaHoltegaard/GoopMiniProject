55cb1c92ca0289fa124e6b699b112d9da1e696a2
Emma <emmaholtegaard@gmail.com>
2024-05-11 12:30:14 +0200

    Initial commit. Created classes.

---
+++ src/Character.java
@@ -0,0 +1,6 @@
+package src;
+
+// Each character should have a name + attributes + image
+
+public class Character {
+}
---
+++ src/Game.java
@@ -0,0 +1,8 @@
+package src;
+
+// This class will hold the game logic.
+// - Logic for filtering based on question asked
+// - A counter or limit on how many question have been/can be asked
+
+public class Game {
+}
